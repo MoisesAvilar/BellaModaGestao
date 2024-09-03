8d0fb7d994c5a4dfc54da811b80a2ab897842959
MoisesAvilar <moisesavilar0@gmail.com>
2024-09-03 10:26:10 -0300

    added __str__ returning supplier and product details

--- stock/models.py
+++ stock/models.py
@@ -6,10 +6,13 @@ class Stock(models.Model):
     supplier = models.CharField(max_length=200)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     amount = models.IntegerField(default=0, editable=False)
-    note = models.TextField(default='null', max_length=200, editable=False)
+    note = models.TextField(default="null", max_length=200, editable=False)
 
     def save(self, *args, **kwargs):
         self.amount = self.product.amount
         self.note = self.product.note
 
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Forncedor: {self.supplier} - Produto: {self.product} {self.note}"
